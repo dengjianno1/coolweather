4c1460b0f86794a1afeb64b0584233ee09f319d2
DengJian <1120409398@qq.com>
2017-05-27 14:30:45 +0800

    加入显示天气信息的功能。

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by dengjian on 2017/5/21.
+ */
+
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
